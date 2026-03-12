d607bb8c2eb933c1deaa170d083f973161c48426
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2026-03-12 08:36:50 -0600

    Optimizador de IA

---
+++ src/main/java/com/mx/curso/Unidad1/optimizacion/DescensoGradiente.java
@@ -0,0 +1,8 @@
+package com.mx.curso.Unidad1.optimizacion;
+
+public class DescensoGradiente extends OptimizadorIA{
+    @Override
+    public String ajustarParametros() {
+        return "Los parámetros se están ajustando...";
+    }
+}
---
+++ src/main/java/com/mx/curso/Unidad1/vehiculo/autonomo/Drone.java
@@ -0,0 +1,8 @@
+package com.mx.curso.Unidad1.vehiculo.autonomo;
+
+public class Drone extends VehiculoAutonomo{
+    @Override
+    public void calcularRuta() {
+        System.out.println("Ruta en 3D");
+    }
+}
