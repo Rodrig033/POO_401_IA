2585927f5bc2a72d66182b15c6dbe0fdefb859e7
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2026-03-18 19:31:54 -0600

    Sensores y polimorfismo

---
+++ src/main/java/com/mx/curso/Unidad1/practica_04/SensorUltrasonido.java
@@ -0,0 +1,8 @@
+package com.mx.curso.Unidad1.practica_04;
+
+public class SensorUltrasonido extends SensorIA{
+    @Override
+    public void leerDatos() {
+        System.out.println("Enviando pulso sónico... Midiendo tiempo de rebote para proximidad.");
+    }
+}
---
+++ src/main/java/com/mx/curso/Unidad1/practica_04/SensorVision.java
@@ -0,0 +1,9 @@
+package com.mx.curso.Unidad1.practica_04;
+
+public class SensorVision extends SensorIA{
+    @Override
+    public void leerDatos() {
+        System.out.println("Capturando frame... Ejecutando segmentación semántica de objetos.");
+    }
+
+}
