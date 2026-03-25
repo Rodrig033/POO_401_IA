ab0ba15d6c283b1e210433847c6b4637306b1ff0
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2026-03-25 08:09:01 -0600

    Modificador de atributos

--- src/main/java/com/mx/curso/Unidad1/practica_04/Main.java
+++ src/main/java/com/mx/curso/Unidad1/practica_04/Main.java
@@ -7,14 +7,15 @@ public class Main {
     public static void main(String[] args) {
         List<SensorIA> percepcionHub = new ArrayList<>();
 
-        SensorLidar objeto1 = new SensorLidar();
-        SensorLidar objeto2 = new SensorLidar();
+        SensorLidar objeto1 = new SensorLidar("1.1", 1.30);
+        SensorLidar objeto2 = new SensorLidar("1.4", 2.65);
 
-        SensorVision objeto3 = new SensorVision();
-        SensorVision objeto4 = new SensorVision();
+        SensorVision objeto3 = new SensorVision("1.23", 2.04);
+        SensorVision objeto4 = new SensorVision("1.33", 3.05);
 
-        SensorUltrasonido objeto5 = new SensorUltrasonido();
-        SensorUltrasonido objeto6 = new SensorUltrasonido();
+        SensorUltrasonido objeto5 = new SensorUltrasonido("2.50", 4.4);
+        SensorUltrasonido objeto6 = new SensorUltrasonido("3.2", 1.2);
+
 
 
         percepcionHub.add(objeto1);
@@ -23,6 +24,12 @@ public class Main {
         percepcionHub.add(objeto4);
         percepcionHub.add(objeto5);
         percepcionHub.add(objeto6);
+        objeto1 = new  SensorLidar("1.2", 1.40);
+        percepcionHub.add(objeto1);
+        objeto2 = new SensorLidar("2.4", 2.67);
+        objeto2.setModelo("2.5");
+        percepcionHub.add(objeto2);
+
 
 
         for (SensorIA sensor: percepcionHub){
