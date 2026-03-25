5cf74e3d341aee8c4cdbaee23205d5fbad89b21f
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2026-03-25 07:42:13 -0600

    Manejo de instancias

--- src/main/java/com/mx/curso/Unidad1/practica_04/Main.java
+++ src/main/java/com/mx/curso/Unidad1/practica_04/Main.java
@@ -6,9 +6,24 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<SensorIA> percepcionHub = new ArrayList<>();
-        percepcionHub.add(new SensorLidar());
-        percepcionHub.add(new SensorVision());
-        percepcionHub.add(new SensorUltrasonido());
+
+        SensorLidar objeto1 = new SensorLidar();
+        SensorLidar objeto2 = new SensorLidar();
+
+        SensorVision objeto3 = new SensorVision();
+        SensorVision objeto4 = new SensorVision();
+
+        SensorUltrasonido objeto5 = new SensorUltrasonido();
+        SensorUltrasonido objeto6 = new SensorUltrasonido();
+
+
+        percepcionHub.add(objeto1);
+        percepcionHub.add(objeto2);
+        percepcionHub.add(objeto3);
+        percepcionHub.add(objeto4);
+        percepcionHub.add(objeto5);
+        percepcionHub.add(objeto6);
+
 
         for (SensorIA sensor: percepcionHub){
             sensor.leerDatos();
