cb8c026cb87f8e1c45451fbba5f053d6652596bc
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2026-04-08 07:33:35 -0600

    Practica 03

---
+++ src/main/java/com/mx/curso/Unidad2/interfaces/practica03/RedNeuronal.java
@@ -0,0 +1,9 @@
+package com.mx.curso.Unidad2.interfaces.practica03;
+
+class RedNeuronal implements SerializableModelo{
+
+    @Override
+    public void guardadEnDisco(String ruta) {
+
+    }
+}
---
+++ src/main/java/com/mx/curso/Unidad2/interfaces/practica03/SerializableModelo.java
@@ -0,0 +1,5 @@
+package com.mx.curso.Unidad2.interfaces.practica03;
+
+public interface SerializableModelo {
+    public void guardadEnDisco(String ruta);
+}
